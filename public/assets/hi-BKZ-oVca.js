6d50a062e28a4da1d2c4620dae5e2281cbc812c7
sqsuqi <643352633@qq.com>
2024-10-25 16:22:11 +0800

    111

--- public/assets/hi-BKZ-oVca.js
+++
@@ -1,1 +0,0 @@
-import{aM as o,c as n,p as r,P as s,l as a,o as c,L as _}from"./index-CR4eVBUJ.js";const l={},u={btn:"",type:"button"};function i(p,t){const e=a("router-link");return c(),n("button",u,[r(e,{to:"/"},{default:s(()=>t[0]||(t[0]=[_(" return ")])),_:1})])}const f=o(l,[["render",i]]);export{f as default};
